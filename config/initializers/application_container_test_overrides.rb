a7cb8257a0b4f6c38290f7e87f5ec4c009352532
Ferrayd <kunitskiisv@gmail.com>
2025-11-15 17:27:41 +0300

    new try

--- config/initializers/application_container_test_overrides.rb
+++ config/initializers/application_container_test_overrides.rb
@@ -1,6 +1,36 @@
 # frozen_string_literal: true
 
 if Rails.env.test?
+  # Полная подмена Octokit-клиента
+  class FakeOctokitClient
+    def initialize(token: nil)
+    end
+
+    def repo(repo_id)
+      {
+        id: repo_id,
+        full_name: "test/repo",
+        default_branch: "main",
+        language: "Ruby"
+      }
+    end
+
+    def repos(*)
+      [
+        {
+          id: 1,
+          full_name: "test/repo1",
+          default_branch: "main",
+          language: "Ruby"
+        }
+      ]
+    end
+  end
+
+  ApplicationContainer.register(:octokit_client) do
+    FakeOctokitClient.new
+  end
+
   ApplicationContainer.register(:fetch_repo_data) do
     ->(_repo_id) do
       {
