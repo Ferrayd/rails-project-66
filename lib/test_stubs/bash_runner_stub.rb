b0fc1250a7004d849eb67b5dcb26d7213531a9b4
Ferrayd <kunitskiisv@gmail.com>
2025-12-29 14:56:44 +0000

    another fixes

--- lib/test_stubs/bash_runner_stub.rb
+++ lib/test_stubs/bash_runner_stub.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-class BashRunnerStub
-  def self.run(_command)
-    # Возвращаем пустой вывод и успешный статус для тестов
-    ['', 0]
+module TestStubs
+  class BashRunnerStub
+    def self.run(_command)
+      ['', 0]
+    end
   end
 end
